c7b84d626ad3d282c81b8def0a4510fce0aec503
Venkatalucky <abburivenkatakalyan1@gmail.com>
2021-12-21 04:46:19 +0530

    added lines

--- Test.java
+++ Test.java
@@ -1,8 +1,16 @@
 this is my 1st project in test
-public class test{
+public class Test{
+
+    public void move()
+    {
+        System.out.println("Move fast");
+    }
     public statis void main(String[] args)
     {
         System.out.println("Main Start");
+        Test t1 = new Test();
+        t1.move();
+
         System.out.println("Main ends");
         
     }
