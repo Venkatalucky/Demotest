db905ace7fb21b0c9ea5dac40ac62a3e4a87fef0
Venkatalucky <abburivenkatakalyan1@gmail.com>
2021-12-20 20:25:19 +0530

    class has created

--- Test.java
+++ Test.java
@@ -1,1 +1,9 @@
 this is my 1st project in test
+public class test{
+    public statis void main(String[] args)
+    {
+        System.out.println("Main Start");
+        System.out.println("Main ends");
+        
+    }
+}
